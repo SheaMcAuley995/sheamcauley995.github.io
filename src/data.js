8822e2538b8aa4ecfa44b0087be76c6b7eae3c4f
Shea McAuley <31017120+SheaMcAuley995@users.noreply.github.com>
2024-05-08 20:01:02 -0700

    Installed yarn and changed main webpage

--- src/data.js
+++ src/data.js
@@ -4,7 +4,7 @@ export const projects = [
     subtitle: "C# | Unity | PlayFab | Firebase | Git | Agile | Scrum | Jira | Confluence | Jenkins | Android | iOS | Game Optimization | Game Monetization ",
     description:
       "Software Engineer II | Lead Engineer",
-    image: "images\TheOfficeBanner.png",
+    image: "images/TheOfficeBanner.png",
     link: "https://play.google.com/store/apps/details?id=com.eastsidegames.theofficeswm&hl=en_US&gl=US",
   },
   {
@@ -12,7 +12,7 @@ export const projects = [
     subtitle: "C++ | Python | Perforce",
     description:
       "Software Engineer Intern",
-    image: "images\WorldOfTanksBanner.png",
+    image: "images/WorldOfTanksBanner.png",
     link: "https://worldoftanks.eu/",
   },
   {
@@ -20,7 +20,7 @@ export const projects = [
     subtitle: "Unity | C# | Game Programming | Unity Imput System | Git | Agile | Scrum | Jira | Confluence | Game Optimization",
     description:
       "Principal Programmer",
-    image: "images\CosmechanicsBanner.png",
+    image: "images/CosmechanicsBanner.png",
     link: "https://cafe-interstellar.itch.io/cosmechanics",
   },
   {
@@ -28,7 +28,7 @@ export const projects = [
     subtitle: "Unity | C# | Game Programming",
     description:
       "Principal Programmer",
-    image: "images\ThumbThumbBanner.png",
+    image: "images/ThumbThumbBanner.png",
     link: "https://elephant-tub-games.itch.io/thumb-maze",
   },
 ];
