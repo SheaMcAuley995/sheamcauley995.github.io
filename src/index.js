c39ad3c2434aeea1861ee1cc749c4c38b309aef0
Shea McAuley <31017120+SheaMcAuley995@users.noreply.github.com>
2024-05-09 13:58:05 -0700

    Update modules and added build

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
 
-ReactDOM.render(
+const domNode = document.getElementById('root');
+const root = createRoot(domNode);
+
+root.render(<App />);
+
+/*const root = createRoot(document.getElementById("root"));
+root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
-);
+);*/
